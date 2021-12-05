9d8f68b95a5b870a5f21f96d6644dbe8ea4ee843
coalross <81717483+coalross@users.noreply.github.com>
2021-12-05 18:27:52 -0500

    Add files via upload

--- Book.java
+++ Book.java
@@ -2,18 +2,18 @@ package library;
 
 import java.io.*;
 
-public class Book /*implements java.io.Serializable*/ {
+public class Book {//implements java.io.Serializable {
 
-	String bookName;
-	String authorName;
-	String available;
-	int volumeNumber;
+	public String bookName;
+	public String authorName;
+	public String available;
+	public int volumeNumber;
 	
 	public Book(String bName, String aName, String avail, int vNum) {
-		String bookName = bName;
-		String authorName = aName;
-		String available = avail;
-		int volumeNumber = vNum;
+		this.bookName = bName;
+		this.authorName = aName;
+		this.available = avail;
+		this.volumeNumber = vNum;
 	}
 	
 	public void setBookName (String bName) {
@@ -28,9 +28,9 @@ public class Book /*implements java.io.Serializable*/ {
 		this.authorName = aName;
 	}
 	
-	public void getAuthorName() {
-		//return authorName;
-		System.out.println(authorName);
+	public String getAuthorName() {
+		return authorName;
+		//System.out.println(authorName);
 	}
 	
 	public void setVolumeNumber(int vNum) {
@@ -48,4 +48,18 @@ public class Book /*implements java.io.Serializable*/ {
 	public String getavailable() {
 		return available;
 	}
+	
+	Book book1 = new Book("Harry Potter", "J.K. Rowling", "Yes", 1);
+	Book book2 = new Book("Harry Potter", "J.K. Rowling", "No", 2);
+	Book book3 = new Book("Harry Potter", "J.K. Rowling", "Yes", 3);
+	Book book4 = new Book("Harry Potter", "J.K. Rowling", "Yes",  4);
+	Book book5 = new Book("Harry Potter", "J.K. Rowling", "No", 5);
+	Book book6 = new Book("Harry Potter", "J.K. Rowling", "Yes", 6);
+	Book book7 = new Book("Harry Potter", "J.K. Rowling", "No", 7);
+	
+	/*public String getAll() {
+		return 
+	}*/
+	
+	
 }
