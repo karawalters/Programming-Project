8ef5c6732d75374936d333e36e48fc02ab411f73
karawalters <81717572+karawalters@users.noreply.github.com>
2021-12-10 21:44:37 -0500

    Update mainGUI.java

--- mainGUI.java
+++ mainGUI.java
@@ -1,4 +1,4 @@
-
+/* C211 Final Project - Team One - Kara Walters - December 10, 2021
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
